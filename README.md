07568b6709aea3a8db85aae90b5f4168116e71ff
Sam Uherek <samuherekbiz@gmail.com>
2024-04-15 08:26:27 +0200

    porgress

---
+++ README.md
@@ -0,0 +1,4 @@
+Seam carving from tsodin's streem to scale images withouth AI
+https://github.com/tsoding/seam-carving/blob/main/main.c
+
+
--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,22 @@ use image::{GenericImageView, ImageBuffer, RgbaImage};
 fn main() {
     let img = image::open("images/Broadway_tower_edit.jpg").expect("Failed to open image");
     let (width, height) = img.dimensions();
-    let mut output: RgbaImage = ImageBuffer::new(width - 100, height);
-    for y in 0..height {
-        let mut splice_x = 0;
-        for x in 0..width {
-            if x < 100 {
-                continue;
+
+    for c in 0..100 {
+        let mut temp = ImageBuffer::new(width - (c + 1), height);
+        for y in 0..height {
+            let mut splice_x = 0;
+            for x in 0..width {
+                if x < 100 {
+                    continue;
+                }
+                let pxl = img.get_pixel(x, y);
+                temp.put_pixel(splice_x, y, pxl);
+                splice_x += 1;
             }
-            let pxl = img.get_pixel(x, y);
-            output.put_pixel(splice_x, y, pxl);
-            splice_x += 1;
+        }
+        if c == 99 {
+            temp.save("output.png").expect("Failed to save image");
         }
     }
-    output.save("output.png").expect("Failed to save image");
 }
